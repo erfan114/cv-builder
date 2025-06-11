ea965de098d12444c9498fb288116c6246b8db96
erfan114 <106922298+erfan114@users.noreply.github.com>
2025-06-11 18:48:31 +0330

    `cdnSource` issue in development server fixed

--- src/modules/core/helper/cdn.helper.ts
+++ src/modules/core/helper/cdn.helper.ts
@@ -3,7 +3,7 @@ import { join } from "path";
 export function cdnSource(src: string) {
   const cdnPath = process.env.NEXT_PUBLIC_CDN_PATH;
 
-  if (!cdnPath) return src;
+  if (!cdnPath || process.env.NODE_ENV !== "production") return src;
 
   return join(cdnPath, src);
 }
