b9ba8f5c5c55115a41ea05a8c8e47062329d66ca
erfan114 <106922298+erfan114@users.noreply.github.com>
2025-06-11 17:25:51 +0330

    Updated `next.config.ts` for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProduction ? "/cv-builder/" : "",
+  basePath: isProduction ? "/cv-builder" : "",
+  output: "export",
 };
 
 export default nextConfig;
