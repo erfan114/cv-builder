8410b2d02bb1c4a7b234c59b6aa6dece44788cb3
erfan114 <106922298+erfan114@users.noreply.github.com>
2025-06-12 13:45:10 +0330

    Add `not-found` page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import { Button, Result } from "antd";
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center">
+      <Result
+        status="404"
+        title="صفحه یافت نشد"
+        subTitle="شرمنده اما صفحه ای که شما به دنبال آن هستید وجود ندارد"
+        extra={
+          <Button
+            type="primary"
+            href="/"
+          >
+            بازگشت به خانه
+          </Button>
+        }
+      />
+    </div>
+  );
+}
