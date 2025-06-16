8338ef60ae9ae8779979c595be2bfe57de298757
erfan114 <106922298+erfan114@users.noreply.github.com>
2025-06-16 18:17:26 +0330

    Added metadata for `templates` and `editor` pages

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -1,4 +1,10 @@
 import { UnderConstruction } from "@/modules/core/components/Construction";
+import { generateDynamicMetadata } from "@/modules/core/helpers/metadata.helper";
+import { Metadata } from "next";
+
+export const metadata: Metadata = generateDynamicMetadata({
+  pageName: "Templates",
+});
 
 export default function Templates() {
   return <UnderConstruction />;
