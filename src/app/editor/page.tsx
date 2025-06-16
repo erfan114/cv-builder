5efda07745aa57ee7a2e54a94451f037bc736842
erfan114 <106922298+erfan114@users.noreply.github.com>
2025-06-16 17:43:06 +0330

    Implemented `editor`, `templates` pages
    - Implemented `UnderConstruction` element for pages that are under development

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,5 @@
+import { UnderConstruction } from "@/modules/core/components/Construction";
+
+export default function Editor() {
+  return <UnderConstruction />;
+}
---
+++ src/modules/core/components/Construction.tsx
@@ -0,0 +1,22 @@
+import { Button } from "antd";
+import { Construction } from "lucide-react";
+import { FC } from "react";
+
+export const UnderConstruction: FC = () => {
+  return (
+    <div className="flex-1 flex flex-col items-center justify-center font-sans gap-1.5 text-center p-5">
+      <Construction className="text-blue-600 size-20" />
+      <h3 className="text-xl">این صفحه در حال توسعه میباشد</h3>
+      <p className="text-sm">
+        با عرض تاسف این صفحه در حال توسعه میباشد، لطفا بعدا دوباره تلاش کنید
+      </p>
+      <Button
+        href="/"
+        variant="filled"
+        color="blue"
+      >
+        <span className="font-sans">بازگشت به صفحه اصلی</span>
+      </Button>
+    </div>
+  );
+};
