64add9c30ba98692674b156f45c4bb5c53905a6d
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 20:03:00 +0330

    feat: refactor Editor component to use pageMetadata and enhance layout structure

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,11 +1,45 @@
-import { UnderConstruction } from "@/modules/core/components/Construction";
-import { generateDynamicMetadata } from "@/modules/core/helpers/metadata.helper";
+import { pageMetadata } from "@/modules/core/helpers/metadata.helper";
+import { DragAndDropContext } from "@/modules/editor/components/DragAndDropContext";
+import { DragAndDropOverlay } from "@/modules/editor/components/DragAndDropOverlay";
+import { EditorEditArea } from "@/modules/editor/components/EditArea";
+import { EditorComponentsSidebar } from "@/modules/editor/components/sidebars/components-sidebar";
+import { EditorPropertiesSidebar } from "@/modules/editor/components/sidebars/properties-sidebar";
+import { ToolBar } from "@/modules/editor/components/toolbar";
+import { ReactFlowProvider } from "@xyflow/react";
+import { Splitter } from "antd";
 import { Metadata } from "next";
 
-export const metadata: Metadata = generateDynamicMetadata({
-  pageName: "Editor",
-});
+export const metadata: Metadata = pageMetadata("Editor");
 
 export default function Editor() {
-  return <UnderConstruction />;
+  return (
+    <ReactFlowProvider>
+      <ToolBar />
+      <div className="flex-1 flex min-h-0">
+        <DragAndDropContext>
+          <DragAndDropOverlay />
+          <Splitter className="flex-1 min-h-0">
+            <Splitter.Panel
+              className="flex overflow-hidden"
+              min={"15%"}
+              defaultSize={"15%"}
+              max={"20%"}
+            >
+              <EditorComponentsSidebar />
+            </Splitter.Panel>
+            <Splitter.Panel className="flex">
+              <EditorEditArea />
+            </Splitter.Panel>
+            <Splitter.Panel
+              min={"15%"}
+              defaultSize={"20%"}
+              max={"20%"}
+            >
+              <EditorPropertiesSidebar />
+            </Splitter.Panel>
+          </Splitter>
+        </DragAndDropContext>
+      </div>
+    </ReactFlowProvider>
+  );
 }
