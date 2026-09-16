bd824e65c754a46190a4d2f3f5c090dc0d7862be
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 20:01:49 +0330

    feat: add builder functions for dropdown items

---
+++ src/modules/editor/components/toolbar/dropdown/item/builder.tsx
@@ -0,0 +1,51 @@
+import { DropdownItemLabel, DropdownItemOptions, DropdownItemType } from ".";
+
+export const buttonDropdownItem = (
+  label: string,
+  key: string,
+  handler: () => void,
+  options?: DropdownItemOptions
+): DropdownItemType => {
+  return {
+    label: (
+      <DropdownItemLabel
+        shortcut={options?.shortcut}
+        key={key}
+      >
+        {label}
+      </DropdownItemLabel>
+    ),
+    key,
+    interact: handler,
+    options,
+  };
+};
+
+export const toggleDropdownItem = (
+  label: string,
+  key: string,
+  state: boolean,
+  onChange: (requestedState: boolean) => void,
+  options?: DropdownItemOptions
+): DropdownItemType => {
+  return {
+    label: (
+      <DropdownItemLabel
+        shortcut={options?.shortcut}
+        checked={state}
+        key={key}
+      >
+        {label}
+      </DropdownItemLabel>
+    ),
+    key,
+    interact: () => {
+      onChange(!state);
+    },
+    options,
+  };
+};
+
+export const dividerDropdownItem = (): DropdownItemType => ({
+  type: "divider",
+});
