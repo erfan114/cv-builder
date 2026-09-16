7d1e15d1252d2958b14ea6d72d749e02d3f84bd2
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 20:02:43 +0330

    feat: add EditTools component with undo and redo functionality

---
+++ src/modules/editor/components/toolbar/tools/Edit.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { ToolbarDropdown } from "../dropdown";
+import { buttonDropdownItem } from "../dropdown/item/builder";
+
+export const EditTools: FC = () => {
+  return (
+    <ToolbarDropdown
+      name="ویرایش"
+      items={[
+        buttonDropdownItem(
+          "برگرداندن",
+          "undo",
+          () => {
+            console.log("Undo called");
+          },
+          {
+            shortcut: "Shift+U",
+          }
+        ),
+        buttonDropdownItem(
+          "باز انجام",
+          "redo",
+          () => {
+            console.log("Redo called");
+          },
+          {
+            shortcut: "Shift+R",
+          }
+        ),
+      ]}
+    />
+  );
+};
