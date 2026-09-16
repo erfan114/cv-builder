2264a7dce16c247e74580f86be906dba369f95d8
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 19:59:09 +0330

    refactor: remove dynamic metadata generation from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import { generateDynamicMetadata } from "@/modules/core/helpers/metadata.helper";
 import { pageWithFooter } from "@/modules/core/helpers/page.helper";
 import { HomeFeatures } from "@/modules/home/components/Features";
 import { HomeHero } from "@/modules/home/components/Hero";
-import { Metadata } from "next";
-
-export const metadata: Metadata = generateDynamicMetadata({ pageName: "Home" });
 
 export default function Home() {
   return pageWithFooter(
