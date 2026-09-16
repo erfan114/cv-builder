f3c6c97c8ecd48cda08a5068cbd81a2e3f31207f
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 19:59:17 +0330

    refactor: replace dynamic metadata generation with static pageMetadata in Templates

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -1,10 +1,8 @@
 import { UnderConstruction } from "@/modules/core/components/Construction";
-import { generateDynamicMetadata } from "@/modules/core/helpers/metadata.helper";
+import { pageMetadata } from "@/modules/core/helpers/metadata.helper";
 import { Metadata } from "next";
 
-export const metadata: Metadata = generateDynamicMetadata({
-  pageName: "Templates",
-});
+export const metadata: Metadata = pageMetadata("Templates");
 
 export default function Templates() {
   return <UnderConstruction />;
