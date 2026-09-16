6ace841f39f0273df75661884ea1b4a67c7561ab
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 20:00:06 +0330

    feat: add DragAndDropOverlay component for drag-and-drop functionality

---
+++ src/modules/editor/components/DragAndDropOverlay.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { DragOverlay, useDndMonitor } from "@dnd-kit/core";
+import { FC, useState } from "react";
+import { DraggableComponent } from "./sidebars/components-sidebar/DraggableComponent";
+
+export const DragAndDropOverlay: FC = () => {
+  const [draggedId, setDraggedId] = useState<string | null>(null);
+
+  useDndMonitor({
+    onDragStart(event) {
+      setDraggedId(event.active.id as string);
+    },
+    onDragEnd() {
+      setDraggedId(null);
+    },
+    onDragCancel() {
+      setDraggedId(null);
+    },
+  });
+
+  return (
+    <DragOverlay>
+      {draggedId ? <DraggableComponent id={draggedId} /> : null}
+    </DragOverlay>
+  );
+};
