c6171ed8916c7ac274edd19ce40f2bc6d8efac7e
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 20:02:19 +0330

    feat: implement useDropdownItems hook for managing dropdown item logic

---
+++ src/modules/editor/hooks/useDropdownItems.ts
@@ -0,0 +1,30 @@
+import { ItemType } from "antd/es/menu/interface";
+import { useMemo } from "react";
+import { DropdownItemType } from "../components/toolbar/dropdown/item";
+
+export const useDropdownItems = (items: DropdownItemType[]) => {
+  return useMemo(() => {
+    const normalizedItems: ItemType[] = [];
+    const handlerMap: { [key: string]: () => void } = {};
+    const keepOpenMap: { [key: string]: boolean } = {};
+    const keyMap: { [key: string]: string } = {};
+    const keyHandlers: { [key: string]: () => void } = {};
+
+    for (const { interact, options, ...rest } of items) {
+      normalizedItems.push(rest);
+      if (!rest.key) continue;
+      const key = rest.key.toString();
+
+      keepOpenMap[key] = options?.keepOpen ?? false;
+
+      if (!interact) continue;
+      handlerMap[key] = interact;
+
+      if (!options?.shortcut) continue;
+      keyMap[key] = options.shortcut;
+      keyHandlers[key] = interact;
+    }
+
+    return { normalizedItems, handlerMap, keepOpenMap, keyMap, keyHandlers };
+  }, [items]);
+};
