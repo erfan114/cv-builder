d71c7a7251b2075992dbd7dc18cffd7e707231fc
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 20:00:54 +0330

    feat: add EditorComponentsSidebar component for draggable elements

---
+++ src/modules/editor/components/sidebars/components-sidebar/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { DraggableComponent } from "./DraggableComponent";
+
+export const EditorComponentsSidebar: FC = () => {
+  return (
+    <div className="p-2 flex flex-col gap-2 flex-1 @container">
+      <div
+        className="grid grid-cols-2 @min-[200px]:grid-cols-3 gap-2 overflow-y-auto h-fit"
+        dir="ltr"
+      >
+        {Array.from({ length: 20 }).map((_, i) => (
+          <DraggableComponent
+            id={`c-${i}`}
+            key={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
