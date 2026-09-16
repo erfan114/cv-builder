4ee5259d3b2c8586a0c8eb48d354a1922d782ebd
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-16 20:01:42 +0330

    feat: add DropdownItemLabel component for customizable dropdown items

---
+++ src/modules/editor/components/toolbar/dropdown/item/index.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/modules/ui/helpers/cn.helper";
+import { ItemType } from "antd/es/menu/interface";
+import { Check } from "lucide-react";
+import { FC, PropsWithChildren } from "react";
+
+export interface DropdownItemOptions {
+  shortcut?: string;
+  keepOpen?: boolean;
+}
+
+export type DropdownItemType = {
+  interact?: () => void;
+  options?: DropdownItemOptions;
+} & ItemType;
+
+export interface DropdownLabelItemProps extends PropsWithChildren {
+  checked?: boolean;
+  shortcut?: string;
+}
+
+export const DropdownItemLabel: FC<DropdownLabelItemProps> = ({
+  children,
+  checked,
+  shortcut,
+}) => {
+  return (
+    <div className="flex items-center gap-2 pe-4 min-w-60">
+      <Check
+        size={16}
+        className={cn("text-blue-600", !checked && "opacity-0")}
+      />
+      <div className={cn("flex-1", shortcut && "pe-5")}>{children}</div>
+      {shortcut && <code className="text-neutral-400 text-xs">{shortcut}</code>}
+    </div>
+  );
+};
