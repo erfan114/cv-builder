1c1ad9009d02d77b6f8f0d8f3961beb1d96b6761
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-17 21:03:47 +0330

    refactor: improve code readability in `githubPath` function

--- src/modules/core/helpers/github.helper.ts
+++ src/modules/core/helpers/github.helper.ts
@@ -3,9 +3,13 @@ import { join } from "node:path";
 export function githubPath(path: string) {
   const cdnPath = process.env.NEXT_PUBLIC_CDN_PATH;
 
-  if (!cdnPath || process.env.NODE_ENV !== "production") return path;
+  if (!cdnPath || process.env.NODE_ENV !== "production") {
+    return path;
+  }
 
-  if (path === "/") return cdnPath;
+  if (path === "/") {
+    return cdnPath;
+  }
 
   return join(cdnPath, path);
 }
